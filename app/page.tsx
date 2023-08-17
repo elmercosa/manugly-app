d88f0e34d6b906f6fccdff16a6468a2c440a60f3
elmer.cortez <35766169+elmercosa@users.noreply.github.com>
2023-08-17 21:45:38 +0200

    rescue project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { ModalLogin } from "@/components/auth/modal-login";
+
+export default function App() {
+  return (
+    <>
+      <ModalLogin />
+    </>
+  );
+}
